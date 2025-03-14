455fe17b8044b32c3ba5a07abf337f54220f7381
Andrew Byles <andrew.byles@fremtind.no>
2025-03-14 15:47:31 +0100

    Refactor doMath from class to math file for class readability

--- src/math.ts
+++ src/math.ts
@@ -1,3 +1,5 @@
+import { actionTypes } from './button-types.ts';
+
 export const add = (a: number, b: number): number => {
     return a + b;
 }
@@ -12,4 +14,19 @@ export const divide = (a: number, b: number): number => {
 
 export const multiply = (a: number, b: number): number => {
     return a * b;
+}
+
+export const doMath = (actionType: actionTypes, totalValue: number, currentValue: number): number => {
+    switch (actionType) {
+        case actionTypes.ADD:
+            return add(totalValue, currentValue);
+        case actionTypes.SUBTRACT:
+            return subtract(totalValue, currentValue);
+        case actionTypes.DIVIDE:
+            return divide(totalValue, currentValue);
+        case actionTypes.MULTIPLY:
+            return multiply(totalValue, currentValue);
+        default:
+            return 0;
+    }
 }
